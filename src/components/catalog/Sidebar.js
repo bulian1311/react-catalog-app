46586b5657af68bf6e4357cf95adb4f3b2d5f6e7
Nikolay Kachanov <bulian1311@gmail.com>
2018-12-07 18:46:14 +0300

    render tags and producers

--- src/components/catalog/Sidebar.js
+++ src/components/catalog/Sidebar.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export class Sidebar extends Component {
+  renderTags = () => {
+    const { tags } = this.props;
+    return tags.map(tag => {
+      return (
+        <div key={tag._id} className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id={tag._id}
+            value={tag.title}
+          />
+          <label className="tag form-check-label" htmlFor={tag._id}>
+            {tag.title}
+          </label>
+        </div>
+      );
+    });
+  };
+
+  renderProducers = () => {
+    const { producers } = this.props;
+    return producers.map(producer => {
+      return (
+        <div key={producer._id} className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id={producer._id}
+            value={producer.url}
+          />
+          <label className="form-check-label" htmlFor={producer._id}>
+            {producer.url}
+          </label>
+        </div>
+      );
+    });
+  };
+
   render() {
     return (
       <div className="collapse text-light" id="navbarToggleExternalContent">
         <div className="bg-primary p-4">
-          <h5 className="text-white h4">Отфильтровыть</h5>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="inlineCheckbox1"
-              value="option1"
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox1">
-              Test tag 1
-            </label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="inlineCheckbox2"
-              value="option2"
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">
-              Test tag 2
-            </label>
-          </div>
+          <h5 className="text-white h4">Поставщики:</h5>
+          {this.props.producers ? this.renderProducers() : ''}
+          <hr />
+          <h5 className="text-white h4">Категории:</h5>
+          {this.props.tags ? this.renderTags() : ''}
         </div>
       </div>
     );
   }
 }
 
-export default Sidebar;
+const mapStateToProps = state => {
+  return {
+    tags: state.tags,
+    producers: state.producers
+  };
+};
+
+export default connect(mapStateToProps)(Sidebar);
